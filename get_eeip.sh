6c29bbf9ca9b0b21504d431c74f4b82a77cc101e
Sabrina Klotz <ysabrinaklotz@web.de>
2024-11-21 16:15:57 +0100

    changed train to test   dmasif/main_inference_exon.py change path   get_eeip.sh updated   requirements.txt

--- get_eeip.sh
+++ get_eeip.sh
@@ -14,17 +14,20 @@ cd dmasif
 mkdir -p surface_data/raw/01-benchmark_surfaces_npy
 
 # Convert PDB to NPY files
-python3 data_preprocessing/convert_pdb2npy.py
+python convert_pdb2npy.py
 
 # Run dMaSIF predictions
-python3 -W ignore -u main_inference_exon.py --experiment_name "$dataset/dMaSIF_search_3layer_12_${fold}_$dataset" --batch_size 1 --embedding_layer dMaSIF --search True --device cuda:"$cuda_no" --radius 12.0 --emb_dim 16 --n_layers 3 --train_no "$fold"
+python -W ignore -u main_inference_exon.py --experiment_name "$dataset/dMaSIF_search_3layer_12_${fold}_$dataset" --batch_size 1 --embedding_layer dMaSIF --search True --device cuda:"$cuda_no" --radius 12.0 --emb_dim 16 --n_layers 3 --train_no "$fold"
 # Run preprocessing for ProteinMAE command: 
 #python -W ignore -u main_inference_exon.py --ppPMAE True --experiment_name "$dataset/dMaSIF_search_3layer_12_${fold}_$dataset" --batch_size 1 --embedding_layer dMaSIF --search True --device cuda:"$cuda_no" --radius 12.0 --emb_dim 16 --n_layers 3 --train_no "$fold"
 # Change directory
 cd ..
 
+# create directory for the npy files if not exists
+mkdir -p results/dMaSIF_DL
+
 # Run PPDL
-python3 /cosybio/project/EEIalpha/EEIpred/ppDL.py -mth dmasif -d "$dataset" -f "$fold"
+python ppDL.py -mth dmasif -d "$dataset" -f "$fold"
 
 # Display the location of the results
 path_results="results/dMaSIF_DL/${dataset}_fold${fold}_results.csv"
